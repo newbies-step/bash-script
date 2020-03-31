ce2152023dd00df683c4d7eb61f8ee7a85b3d19a
Ajil Raju, 0xA <31854172+ajilraju@users.noreply.github.com>
2020-03-31 06:46:50 +0530

    Added the random things, with example

---
+++ random-things/useof-case.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+havefruit="apple"
+
+case $havefruit in 
+
+	"orange") 
+				echo "You have Orange";;
+	"mango")	
+				echo "You have Mango";;
+	"apple")	
+				echo "You have Apple";;
+	*)
+				echo "You have nothing!!!";;
+esac
