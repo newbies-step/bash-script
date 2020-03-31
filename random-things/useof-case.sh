60cae42d7c035afde75928b8c632a5f7d1fff3d5
Ajil Raju, 0xA <31854172+ajilraju@users.noreply.github.com>
2020-03-31 06:49:20 +0530

    case example formatted

--- random-things/useof-case.sh
+++ random-things/useof-case.sh
@@ -4,12 +4,12 @@ havefruit="apple"
 
 case $havefruit in 
 
-	"orange") 
-				echo "You have Orange";;
-	"mango")	
-				echo "You have Mango";;
-	"apple")	
-				echo "You have Apple";;
-	*)
-				echo "You have nothing!!!";;
+    "orange") 
+         echo "You have Orange";;
+    "mango")	
+         echo "You have Mango";;
+    "apple")	
+         echo "You have Apple";;
+    *)
+         echo "You have nothing!!!";;
 esac
